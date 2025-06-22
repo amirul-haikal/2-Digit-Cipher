cba117e6266446e377d258a8f37b274cfb2e7dcb
amirul-haikal <amirulhaikal932@gmail.com>
2025-06-22 15:38:25 +0800

    new

--- cipher/old_code.py
+++ cipher/old_code.py
@@ -7,25 +7,25 @@ key_map = {
     'Z': '25'
 }
 
-reverse_map = {
-    '00': 'A', '01': 'B', '02': 'C', '03': 'D', '04': 'E',
-    '05': 'F', '06': 'G', '07': 'H', '08': 'I', '09': 'J',
-    '10': 'K', '11': 'L', '12': 'M', '13': 'N', '14': 'O',
-    '15': 'P', '16': 'Q', '17': 'R', '18': 'S', '19': 'T',
-    '20': 'U', '21': 'V', '22': 'W', '23': 'X', '24': 'Y',
-    '25': 'Z'
-}
-
+reverse_map = {v: k for k, v in key_map.items()}
 
 def encrypt(text):
-    text = text.upper().replace(" ", "")
-    encrypted = [key_map.get(char, '??') for char in text]
-    return ' '.join(encrypted)
+    text = text.upper()
+    words = text.split()
+    encrypted_words = []
+    for word in words:
+        encrypted_word = ''.join([key_map.get(char, char) for char in word])
+        encrypted_words.append(encrypted_word)
+    return ' '.join(encrypted_words)
 
 def decrypt(cipher_text):
-    codes = cipher_text.strip().split()
-    decrypted = [reverse_map.get(code, '?') for code in codes]
-    return ''.join(decrypted)
+    words = cipher_text.strip().split()
+    decrypted_words = []
+    for word in words:
+        chars = [word[i:i+2] for i in range(0, len(word), 2)]
+        decrypted_word = ''.join([reverse_map.get(code, code) for code in chars])
+        decrypted_words.append(decrypted_word)
+    return ' '.join(decrypted_words)
 
 # --- Example Usage ---
 if __name__ == "__main__":
@@ -35,6 +35,6 @@ if __name__ == "__main__":
     cipher_result = encrypt(plain_text)
     print("Encrypted:", cipher_result)
 
-    cipher_input = input("\nEnter 2-digit codes to decrypt (separated by space): ")
+    cipher_input = input("\nEnter 2-digit codes to decrypt (space between words): ")
     decrypted_text = decrypt(cipher_input)
     print("Decrypted:", decrypted_text)
